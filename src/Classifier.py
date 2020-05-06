1983a68b3bcba9e39ada0e0fbebd332a40cd594e
Achchu <archulan>
2020-05-06 21:57:58 +0530

    columns added

--- src/Classifier.py
+++ src/Classifier.py
@@ -4,3 +4,8 @@ from sklearn.tree import DecisionTreeClassifier # Import Decision Tree Classifie
 from sklearn.model_selection import train_test_split # Import train_test_split function
 from sklearn import metrics #Import scikit-learn metrics module for accuracy calculation
 
+col_names = ['tripid', 'additional_fare', 'duration', 'meter_waiting', 'meter_waiting_fare', 'meter_waiting_till_pickup', 'pickup_time', 'drop_time', 'pick_lat', 'pick_lon', 'drop_lat', 'drop_lon', 'fare', 'label']
+# load dataset
+data = pd.read_csv('train.csv')
+
+data.head()
