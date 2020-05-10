211282ba8a1c7211b2e7e81b6e89f60ea18fc696
Achchu <archulan>
2020-05-10 20:36:40 +0530

    Label converted to binary

--- src/Classifier.py
+++ src/Classifier.py
@@ -1,8 +1,9 @@
+import numpy as np
 import pandas as pd
 from xgboost import XGBClassifier
-from sklearn.model_selection import train_test_split  # Import train_test_split function
+from sklearn import preprocessing
 from sklearn import metrics  # Import scikit-learn metrics module for accuracy calculation
-import numpy as np
+from sklearn.model_selection import train_test_split  # Import train_test_split function
 
 column_names = ['tripid', 'additional_fare', 'duration', 'meter_waiting', 'meter_waiting_fare',
                 'meter_waiting_till_pickup', 'pickup_time', 'drop_time', 'pick_lat', 'pick_lon', 'drop_lat', 'drop_lon',
@@ -13,25 +14,26 @@ training_data = pd.read_csv('train.csv')
 training_data = training_data.dropna()
 testing_data = pd.read_csv('test.csv')
 
-print(training_data.head())
-
 # split training dataset into feature and target variables
 feature_columns = ['additional_fare', 'duration', 'meter_waiting', 'meter_waiting_fare',
                 'meter_waiting_till_pickup', 'pick_lat', 'pick_lon', 'drop_lat', 'drop_lon',
                 'fare']
 
 x_train = training_data[feature_columns]
+
+# lb = preprocessing.LabelBinarizer()
+# print(training_data.label)
+# y_train = lb.fit_transform(training_data.label)
+labelToBinary = {'correct': 1, 'incorrect': 0}
+training_data.label = [labelToBinary[item] for item in training_data.label]
+print(training_data)
 y_train = training_data.label
 
 x_train, x_test, y_train, y_test = train_test_split(x_train, y_train, test_size=0.2, random_state=1)
 # x_test = testing_data[feature_columns]
 
-# shuffle_index = np.random.permutation(13574)
-# print(len(x_train), 'length', len(y_train))
-# x_train, y_train = x_train[shuffle_index], y_train[shuffle_index]
-
 # create classifier
-clf = XGBClassifier()
+clf = XGBClassifier(learning_rate=0.25, min_child_weight=10, gamma=2, seed=1)
 
 # train classifier
 clf = clf.fit(x_train, y_train)
@@ -39,4 +41,10 @@ clf = clf.fit(x_train, y_train)
 # classify the ride fare
 y_predict = clf.predict(x_test)
 
+print(y_test,'tes', y_predict,'pre')
 print('Accu:', metrics.accuracy_score(y_test, y_predict))
+#
+# df = pd.DataFrame(y_predict, columns=['prediction'], index=testing_data['tripid'])
+# df.index.name = 'tripid'
+#
+# df.to_csv('160040d_submission_1')
