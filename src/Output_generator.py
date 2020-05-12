8e7f18a38a06c8ca91021f9f46ed8aac696b304a
Achchu <archulan>
2020-05-12 21:47:40 +0530

    Output generator seperated

--- src/Output_generator.py
+++ src/Output_generator.py
@@ -1,7 +1,5 @@
 import pandas as pd
-from sklearn import metrics  # Import scikit-learn metrics module for accuracy calculation
 from xgboost import XGBClassifier
-from sklearn.model_selection import train_test_split  # Import train_test_split function
 
 column_names = ['tripid', 'additional_fare', 'duration', 'meter_waiting', 'meter_waiting_fare',
                 'meter_waiting_till_pickup', 'pickup_time', 'drop_time', 'pick_lat', 'pick_lon', 'drop_lat', 'drop_lon',
@@ -23,11 +21,6 @@ labelToBinary = {'correct': 1, 'incorrect': 0}
 training_data.label = [labelToBinary[item] for item in training_data.label]
 y_train = training_data.label
 
-x_train, x_test, y_train, y_test = train_test_split(x_train, y_train, test_size=0.2, random_state=1)
-print(x_train,'x_train', x_test,'x_test', y_train,'y_train', y_test,'y_test')
-
-train, validate, test = np.split(df.sample(frac=1), [int(.6*len(df)), int(.8*len(df))])
-# x_train, x_validate, y_train, y_validate = train_test_split(x_train, y_train, test_size=0.25, random_state=1)
 # x_test = testing_data[feature_columns]
 
 # create classifier
@@ -39,9 +32,8 @@ clf = clf.fit(x_train, y_train)
 # classify the ride fare
 y_predict = clf.predict(x_test)
 
-print('Accu:', metrics.accuracy_score(y_test, y_predict))
-#
-# df = pd.DataFrame(y_predict, columns=['prediction'], index=testing_data['tripid'])
-# df.index.name = 'tripid'
-#
-# df.to_csv('output/160040d_submission_1')
+
+df = pd.DataFrame(y_predict, columns=['prediction'], index=testing_data['tripid'])
+df.index.name = 'tripid'
+
+df.to_csv('output/160040d_submission_1')
