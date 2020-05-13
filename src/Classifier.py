f9c20bb62bbeb65e4b130e4ffa771570e34b172c
Achchu <archulan>
2020-05-13 12:28:20 +0530

    Data preprocessing init

--- src/Classifier.py
+++ src/Classifier.py
@@ -1,21 +1,36 @@
 import pandas as pd
+import csv
+import datetime
 from sklearn import metrics  # Import scikit-learn metrics module for accuracy calculation
 from xgboost import XGBClassifier
 from sklearn.model_selection import train_test_split  # Import train_test_split function
 
 column_names = ['tripid', 'additional_fare', 'duration', 'meter_waiting', 'meter_waiting_fare',
                 'meter_waiting_till_pickup', 'pickup_time', 'drop_time', 'pick_lat', 'pick_lon', 'drop_lat', 'drop_lon',
-                'fare', 'label']
+                'fare', 'label', 'delta_lat']
 
+
+def compute_column(csv_file):
+    with open(csv_file, newline='') as f:
+        with open('refined_train.csv', 'w', newline='') as f2:
+            writer = csv.writer(f2)
+            rows = csv.reader(f)
+            for row in rows:
+                y = []
+                y.append(float(row[8]) - float(row[10]))
+                writer.writerow(row + y)
+
+
+# compute_column('train.csv')
 # loading dataset
-training_data = pd.read_csv('train.csv')
+training_data = pd.read_csv('refined_train.csv')
 training_data = training_data.dropna()
 testing_data = pd.read_csv('test.csv')
 
 # split training dataset into feature and target variables
 feature_columns = ['additional_fare', 'duration', 'meter_waiting', 'meter_waiting_fare',
-                'meter_waiting_till_pickup', 'pick_lat', 'pick_lon', 'drop_lat', 'drop_lon',
-                'fare']
+                   'meter_waiting_till_pickup', 'pick_lat', 'pick_lon', 'drop_lat', 'drop_lon',
+                   'fare', 'delta_lat']
 
 x_train = training_data[feature_columns]
 
@@ -24,11 +39,10 @@ training_data.label = [labelToBinary[item] for item in training_data.label]
 y_train = training_data.label
 
 x_train, x_test, y_train, y_test = train_test_split(x_train, y_train, test_size=0.2, random_state=1)
-print(x_train,'x_train', x_test,'x_test', y_train,'y_train', y_test,'y_test')
+print(x_train, 'x_train', x_test, 'x_test', y_train, 'y_train', y_test, 'y_test')
 
-train, validate, test = np.split(df.sample(frac=1), [int(.6*len(df)), int(.8*len(df))])
+# train, validate, test = np.split(df.sample(frac=1), [int(.6*len(df)), int(.8*len(df))])
 # x_train, x_validate, y_train, y_validate = train_test_split(x_train, y_train, test_size=0.25, random_state=1)
-# x_test = testing_data[feature_columns]
 
 # create classifier
 clf = XGBClassifier(learning_rate=0.25, min_child_weight=10, gamma=2, seed=1)
@@ -40,8 +54,3 @@ clf = clf.fit(x_train, y_train)
 y_predict = clf.predict(x_test)
 
 print('Accu:', metrics.accuracy_score(y_test, y_predict))
-#
-# df = pd.DataFrame(y_predict, columns=['prediction'], index=testing_data['tripid'])
-# df.index.name = 'tripid'
-#
-# df.to_csv('output/160040d_submission_1')
--- src/Output_generator.py
+++ src/Output_generator.py
@@ -1,27 +1,43 @@
 import pandas as pd
+import csv
 from xgboost import XGBClassifier
+
+def compute_column(csv_file):
+    with open(csv_file, newline='') as f:
+        with open('refined_test.csv', 'w', newline='') as f2:
+            writer = csv.writer(f2)
+            rows = csv.reader(f)
+            for row in rows:
+                y = []
+                y.append(float(row[8]) - float(row[10]))
+                writer.writerow(row + y)
 
 column_names = ['tripid', 'additional_fare', 'duration', 'meter_waiting', 'meter_waiting_fare',
                 'meter_waiting_till_pickup', 'pickup_time', 'drop_time', 'pick_lat', 'pick_lon', 'drop_lat', 'drop_lon',
                 'fare', 'label']
 
+# compute_column('test.csv')
 # loading dataset
-training_data = pd.read_csv('train.csv')
+training_data = pd.read_csv('refined_train.csv')
 training_data = training_data.dropna()
-testing_data = pd.read_csv('test.csv')
+testing_data = pd.read_csv('refined_test.csv')
 
 # split training dataset into feature and target variables
-feature_columns = ['additional_fare', 'duration', 'meter_waiting', 'meter_waiting_fare',
+training_feature_columns = ['additional_fare', 'duration', 'meter_waiting', 'meter_waiting_fare',
                 'meter_waiting_till_pickup', 'pick_lat', 'pick_lon', 'drop_lat', 'drop_lon',
-                'fare']
+                'fare', 'delta_lat']
 
-x_train = training_data[feature_columns]
+testing_feature_columns = ['additional_fare', 'duration', 'meter_waiting', 'meter_waiting_fare',
+                'meter_waiting_till_pickup', 'pick_lat', 'pick_lon', 'drop_lat', 'drop_lon',
+                'fare', 'delta_lat']
+
+x_train = training_data[training_feature_columns]
 
 labelToBinary = {'correct': 1, 'incorrect': 0}
 training_data.label = [labelToBinary[item] for item in training_data.label]
 y_train = training_data.label
 
-# x_test = testing_data[feature_columns]
+x_test = testing_data[testing_feature_columns]
 
 # create classifier
 clf = XGBClassifier(learning_rate=0.25, min_child_weight=10, gamma=2, seed=1)
@@ -32,8 +48,7 @@ clf = clf.fit(x_train, y_train)
 # classify the ride fare
 y_predict = clf.predict(x_test)
 
-
 df = pd.DataFrame(y_predict, columns=['prediction'], index=testing_data['tripid'])
 df.index.name = 'tripid'
 
-df.to_csv('output/160040d_submission_1')
+df.to_csv('160040d_submission_2')
