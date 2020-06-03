34f0af4815965872b01e5ebd69a93ebcf61df212
Achchu <archulan>
2020-06-03 11:29:22 +0530

    submission_16_delta_time_removed

--- src/Output_generator.py
+++ src/Output_generator.py
@@ -4,7 +4,8 @@ import pandas as pd
 from datetime import datetime
 from xgboost import XGBClassifier
 
-def compute_column(csv_file):
+
+def preprocess_train(csv_file):
     with open(csv_file, newline='') as f:
         with open('refined_train.csv', 'w', newline='') as f2:
             writer = csv.writer(f2)
@@ -12,39 +13,81 @@ def compute_column(csv_file):
             r = 6373.0
             count = 0
             for row in rows:
-              if count == 0:
-                writer.writerow(row[1:6] + row[8:] + ['distance'] + ['delta_time'] + ['travel_time'])
-              elif count > 0:
-                lat1 = math.radians(float(row[8]))
-                lat2 = math.radians(float(row[10]))
-                delta_lat = abs(lat1 - lat2)
-                delta_lon = abs(math.radians(float(row[9])) - math.radians(float(row[11])))
-                a = math.sin(delta_lat / 2) ** 2 + math.cos(lat1) * math.cos(lat2) * math.sin(delta_lon / 2) ** 2
-                c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
-                distance = r * c
+                if count == 0:
+                    writer.writerow(row[1:6] + row[8:] + ['distance'] + ['travel_time'] + ['trip_day'])
+                elif count > 0:
+                    lat1 = math.radians(float(row[8]))
+                    lat2 = math.radians(float(row[10]))
+                    delta_lat = abs(lat1 - lat2)
+                    delta_lon = abs(math.radians(float(row[9])) - math.radians(float(row[11])))
+                    a = math.sin(delta_lat / 2) ** 2 + math.cos(lat1) * math.cos(lat2) * math.sin(delta_lon / 2) ** 2
+                    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+                    distance = r * c
 
-                pickup_time = datetime.strptime(row[6], '%m/%d/%Y %H:%M')
-                drop_time = datetime.strptime(row[7], '%m/%d/%Y %H:%M')
-                delta_time = abs((drop_time - pickup_time).seconds)
+                    # pickup_time = datetime.strptime(row[6], '%m/%d/%Y %H:%M')
+                    # drop_time = datetime.strptime(row[7], '%m/%d/%Y %H:%M')
+                    # delta_time = abs((drop_time - pickup_time).seconds)
 
-                if row[2] == '':
-                  duration = 0
-                else:
-                  duration = int(row[2])
-                if row[3] == '':
-                  meter_waiting = 0
-                else:
-                  meter_waiting = int(row[3])
+                    if row[2] == '':
+                        duration = 0
+                    else:
+                        duration = int(row[2])
+                    if row[3] == '':
+                        meter_waiting = 0
+                    else:
+                        meter_waiting = int(row[3])
 
-                travel_time = duration - meter_waiting
+                    travel_time = duration - meter_waiting
 
-                writer.writerow(row[1:6] + row[8:] + [distance] + [delta_time] + [travel_time])
-              count += 1
-column_names = ['tripid', 'additional_fare', 'duration', 'meter_waiting', 'meter_waiting_fare',
-                'meter_waiting_till_pickup', 'pickup_time', 'drop_time', 'pick_lat', 'pick_lon', 'drop_lat', 'drop_lon',
-                'fare', 'label']
+                    pickup_time = datetime.strptime(row[6], '%m/%d/%Y %H:%M')  # drop day also available
+                    trip_day = pickup_time.strftime('%w')
 
-# compute_column('test.csv')
+                    writer.writerow(row[1:6] + row[8:] + [distance] + [travel_time] + [trip_day])
+                count += 1
+
+def preprocess_test(csv_file):
+    with open(csv_file, newline='') as f:
+        with open('refined_test.csv', 'w', newline='') as f2:
+            writer = csv.writer(f2)
+            rows = csv.reader(f)
+            r = 6373.0
+            count = 0
+            for row in rows:
+                if count == 0:
+                    writer.writerow(row[:6] + row[8:] + ['distance'] + ['travel_time'] + ['trip_day'])
+                elif count > 0:
+                    lat1 = math.radians(float(row[8]))
+                    lat2 = math.radians(float(row[10]))
+                    delta_lat = abs(lat1 - lat2)
+                    delta_lon = abs(math.radians(float(row[9])) - math.radians(float(row[11])))
+                    a = math.sin(delta_lat / 2) ** 2 + math.cos(lat1) * math.cos(lat2) * math.sin(delta_lon / 2) ** 2
+                    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+                    distance = r * c
+
+                    # pickup_time = datetime.strptime(row[6], '%m/%d/%Y %H:%M')
+                    # drop_time = datetime.strptime(row[7], '%m/%d/%Y %H:%M')
+                    # delta_time = abs((drop_time - pickup_time).seconds)
+
+                    if row[2] == '':
+                        duration = 0
+                    else:
+                        duration = int(row[2])
+                    if row[3] == '':
+                        meter_waiting = 0
+                    else:
+                        meter_waiting = int(row[3])
+
+                    travel_time = duration - meter_waiting
+
+                    pickup_time = datetime.strptime(row[6], '%m/%d/%Y %H:%M')  # drop day also available
+                    trip_day = pickup_time.strftime('%w')
+
+                    writer.writerow(row[:6] + row[8:] + [distance] + [travel_time] + [trip_day])
+                count += 1
+
+
+preprocess_train('train.csv')
+preprocess_test('test.csv')
 # loading dataset
 dataset = pd.read_csv('refined_train.csv')
 training_data = pd.DataFrame(dataset).fillna(dataset.mean())
@@ -53,12 +96,12 @@ testing_data = pd.read_csv('refined_test.csv')
 
 # split training dataset into feature and target variables
 training_feature_columns = ['additional_fare', 'duration', 'meter_waiting', 'meter_waiting_fare',
-                'meter_waiting_till_pickup', 'pick_lat', 'pick_lon', 'drop_lat', 'drop_lon',
-                'fare','distance', 'delta_time']
+                            'meter_waiting_till_pickup', 'pick_lat', 'pick_lon', 'drop_lat', 'drop_lon',
+                            'fare', 'distance', 'travel_time', 'trip_day']
 
 testing_feature_columns = ['additional_fare', 'duration', 'meter_waiting', 'meter_waiting_fare',
-                'meter_waiting_till_pickup', 'pick_lat', 'pick_lon', 'drop_lat', 'drop_lon',
-                'fare','distance', 'delta_time']
+                           'meter_waiting_till_pickup', 'pick_lat', 'pick_lon', 'drop_lat', 'drop_lon',
+                           'fare', 'distance', 'travel_time', 'trip_day']
 
 x_train = training_data[training_feature_columns]
 
@@ -77,8 +120,7 @@ x_test = testing_data[testing_feature_columns]
 #                     gamma=1, base_score=0.5)
 clf = XGBClassifier(booster='gbtree', learning_rate=0.25000008, gamma=0, max_depth=25,
                     min_child_weight=0, max_delta_step=0, subsample=1, colsample_bytree=1,
-                    colsample_bylevel=1, colsample_bynode=1, reg_lambda=2.0005  )
-
+                    colsample_bylevel=1, colsample_bynode=1, reg_lambda=2.0005)
 
 # train classifier
 clf = clf.fit(x_train, y_train)
@@ -89,4 +131,4 @@ y_predict = clf.predict(x_test)
 df = pd.DataFrame(y_predict, columns=['prediction'], index=testing_data['tripid'])
 df.index.name = 'tripid'
 
-df.to_csv('160040d_submission_15')
+df.to_csv('160040d_submission_16')
